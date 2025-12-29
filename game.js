bff750b450c27d34079d9486c74c2682ff48ef26
welly356 <acharyasarthak001@gmail.com>
2025-12-29 15:08:29 -0700

    Update socket connection to use websocket transport

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const REPLIT_URL = "https://.thornton-break-in.acharyasarthak0.replit.app" 
-const socket = io(REPLIT_URL); 
+const socket = io(REPLIT_URL, { transports: ['websocket'] });
 
 let scene, camera, renderer, peer, conn, flashlight;
 let move = { f: false, b: false, l: false, r: false, interact: false };
